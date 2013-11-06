ac82c7105e6eff7594996a80114dfd0887eb26e6
srinivas.kancharla <sk@headwire.com>
2013-11-06 14:43:25 -0800

    Adding Cookbooks

---
+++ cookbooks/basedevserv/metadata.rb
@@ -0,0 +1,10 @@
+name             'basedevserv'
+maintainer       'Headwire Inc.'
+maintainer_email 'sk@headwire.com'
+license          'All rights reserved'
+description      'Installs/Configures basedevserv'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "java"
+depends "tomcat_latest"
---
+++ cookbooks/basedevserv/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: basedevserv
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "java"
+include_recipe "tomcat_latest"
