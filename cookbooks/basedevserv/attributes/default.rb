07962214c646153554b18364dbca4cdb424a61bb
srinivas.kancharla <sk@headwire.com>
2013-11-11 13:59:40 -0800

    Adding openldap server

--- cookbooks/basedevserv/attributes/default.rb
+++ cookbooks/basedevserv/attributes/default.rb
@@ -1,3 +1,7 @@
+# Oracle JDK Related Config Attributes
 node.default['java']['install_flavor'] = "oracle"
 node.default['java']['jdk_version'] = '7'
 node.default['java']['oracle']['accept_oracle_download_terms'] = true
+
+# Open LDAP Related Config attributes
+node['domain'] = "example.com"
