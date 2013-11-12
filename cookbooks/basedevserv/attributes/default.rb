82561f1f59b916d581edd71032af054fd0afa211
srinivas.kancharla <sk@headwire.com>
2013-11-12 11:28:07 -0800

    Adding openldap server

--- cookbooks/basedevserv/attributes/default.rb
+++ cookbooks/basedevserv/attributes/default.rb
@@ -4,10 +4,7 @@ node.default['java']['jdk_version'] = '7'
 node.default['java']['oracle']['accept_oracle_download_terms'] = true
 
 # Open LDAP Related Config attributes
-node.default['openldap-server']['domain'] = "example.com"
-node.default['openldap-server']['rootpw'] = "password"
-node.default['openldap-server']['root_user_attr'] = "cn=admin"
-#node.default['domain'] = "example.com"
-#node.default['openldap']['rootpw'] = 'password'
-#node.default['openldap']['basedn'] = 'example.com'
+node.default['domain'] = "example.com"
+node.default['openldap']['rootpw'] = 'password'
+node.default['openldap']['basedn'] = 'example.com'
 #node.default['openldap']['server'] = "ldap.example.com"
